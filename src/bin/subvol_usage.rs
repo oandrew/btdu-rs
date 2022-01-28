4b191984526a17bb92c265d94fd09a6e07d95986
Andrew Onyshchuk <andryk.rv@gmail.com>
2022-01-27 20:47:03 -0800

    wip

--- src/bin/subvol_usage.rs
+++ src/bin/subvol_usage.rs
@@ -1,3 +1,5 @@
+#![feature(stdio_locked)]
+
 use std::{collections::{HashMap, HashSet, VecDeque}, env, hash::{BuildHasher, Hasher}, alloc::Layout, ops::{Deref, DerefMut, Range, RangeInclusive, AddAssign}, ffi::{CStr, CString}, fmt, io::Write, rc::Rc, borrow::Borrow, time::Duration, marker::PhantomData};
 
 use nix::{fcntl::{self, OFlag}, libc::{self, c_char}, sys::stat::Mode};
@@ -5,7 +7,7 @@ use nix::NixPath;
 use anyhow::Result;
 use clap::Parser;
 
-use btdu_rs::btrfs;
+use btdu_rs::{btrfs, SampleTree};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -64,16 +66,12 @@ impl <'a>  PtrReader<'a> {
     }
 }
 
-unsafe fn get_and_move_typed<T: Sized>(ptr: &mut *const u8) -> *const T {
-    let res = *ptr as *const T;
-    *ptr = (*ptr).add(std::mem::size_of::<T>());
-    res
-}
-
 fn main() -> Result<()> {
     let args = Args::parse();
     // let args: Vec<_> = env::args().collect();
     let fd = fcntl::open(args.path.as_str(), OFlag::O_RDONLY, Mode::empty())?;
+
+    let mut root_usage: HashMap<(u64, u64), u64> = HashMap::new();
 
     btrfs::tree_search_cb(fd, btrfs::BTRFS_EXTENT_TREE_OBJECTID as u64, btrfs::SearchKey::ALL, |sh, data| {
         let mut r = PtrReader::new(data);
@@ -85,26 +83,29 @@ fn main() -> Result<()> {
                 } else {
                     None
                 };
-                println!("({} BTRFS_EXTENT_ITEM_KEY {:6}) {:?} {:?}", sh.objectid, sh.offset, extent_item, tree_block_info);
+                // println!("({} BTRFS_EXTENT_ITEM_KEY {:6}) {:?} {:?}", sh.objectid, sh.offset, extent_item, tree_block_info);
+
+                let extent_length = sh.offset;
 
                 if (extent_item.flags & btrfs::BTRFS_EXTENT_FLAG_DATA as u64) != 0 {
                     while let Some(extent_inline_ref_type) = r.next::<u8>() {
                         match *extent_inline_ref_type as u32 {
                             btrfs::BTRFS_EXTENT_DATA_REF_KEY => {
                                 let extent_inline_data_ref = r.next::<btrfs::btrfs_extent_data_ref>().unwrap();
-                                println!("  BTRFS_EXTENT_DATA_REF_KEY {:?}", extent_inline_data_ref);
+                                // println!("  BTRFS_EXTENT_DATA_REF_KEY {:?}", extent_inline_data_ref);
+
+                                root_usage.entry((extent_inline_data_ref.root, extent_inline_data_ref.objectid)).or_default().add_assign(extent_length);
                                 // *root_usage.entry(extent_inline_data_ref.root).or_default() += search_header.offset;
                             },
 
                             btrfs::BTRFS_SHARED_DATA_REF_KEY => {
                                 let _extent_inline_ref_offset = r.next::<u64>().unwrap();
                                 let shared_data_ref = r.next::<btrfs::btrfs_shared_data_ref>().unwrap();
-                                println!("  BTRFS_SHARED_DATA_REF_KEY {:?}", shared_data_ref);
+                                // println!("  BTRFS_SHARED_DATA_REF_KEY {:?}", shared_data_ref);
                             },
                             _ => {
                                 // println!("  type = {}", extent_inline_ref.type_);
-                                println!("BTRFS_EXTENT_ITEM_KEY / DATA / extent_inline_ref.type={}", extent_inline_ref_type);
-                                break;
+                                todo!("BTRFS_EXTENT_ITEM_KEY / DATA / extent_inline_ref.type={}", extent_inline_ref_type);
                             },
                         }
 
@@ -117,7 +118,7 @@ fn main() -> Result<()> {
             },
             btrfs::BTRFS_METADATA_ITEM_KEY  => {
                 let extent_item = r.next::<btrfs::btrfs_extent_item>().unwrap();
-                println!("({} BTRFS_METADATA_ITEM_KEY {:6}) {:?}", sh.objectid, sh.offset, extent_item);
+                // println!("({} BTRFS_METADATA_ITEM_KEY {:6}) {:?}", sh.objectid, sh.offset, extent_item);
 
                 while let Some(extent_inline_ref) = r.next::<btrfs::btrfs_extent_inline_ref>() {
                     // println!("  {:?}", extent_inline_ref);
@@ -131,7 +132,7 @@ fn main() -> Result<()> {
                             //     Some(usage) => *root_usage.entry(root).or_default() += usage,
                             //     None => {},
                             // }
-                            println!("  BTRFS_TREE_BLOCK_REF_KEY");
+                            // println!("  BTRFS_TREE_BLOCK_REF_KEY");
                         },
                         btrfs::BTRFS_SHARED_BLOCK_REF_KEY  => {
                             // let parent = extent_inline_ref.offset;
@@ -139,7 +140,7 @@ fn main() -> Result<()> {
                             //     Some(root) => *root_usage.entry(*root).or_default() += 16*1024,
                             //     None => *unresolved_usage.entry(parent).or_default() += 16*1024,
                             // }
-                            println!("  BTRFS_SHARED_BLOCK_REF_KEY");
+                            // println!("  BTRFS_SHARED_BLOCK_REF_KEY");
                         },
                         _ => {
                             todo!();
@@ -159,6 +160,32 @@ fn main() -> Result<()> {
 
 
     })?;
+
+    // println!("{:?}", root_usage);
+
+    let mut sample_tree = SampleTree::new();
+
+    let mut total: u64 = 0;
+    for (k, v) in &root_usage {
+        match &btrfs::ino_lookup_sync(fd, k.0,k.1) {
+            Ok(path) => {
+                //println!("{:>80} {}", path, v);
+                sample_tree.add_samples(path.split('/'), *v);
+                total += v;
+            },
+            Err(_) => {
+                println!("Could not resolve {:?}", k);
+            },
+        }
+    }
+
+    println!("Unique inodes: {}", root_usage.len());
+
+    let mut buf = String::new();
+    sample_tree.print(&mut buf, total, 1.0, Some(0.01))?;
+    // sample_tree.print(&mut buf, total, 1.0, None)?;
+    // sample.print(&mut buf, bytes_per_sample, Some(args.min_pct / 100.0))?;
+    std::io::stdout_locked().write_all(buf.as_bytes())?;
 
     
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,123 @@
 #![feature(bigint_helper_methods)]
 #![feature(maybe_uninit_slice)]
 #![feature(maybe_uninit_uninit_array)]
+#![feature(hash_raw_entry)]
+
+use std::{collections::HashMap, fmt};
 
 
 pub mod btrfs;
+
+
+pub struct SampleTree {
+    total: u64,
+    children: HashMap<String, SampleTree>,
+}
+
+impl Default for SampleTree {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl SampleTree {
+    pub fn new() -> Self {
+        Self {
+            total: 0,
+            children: HashMap::new(),
+        }
+    }
+
+    // fn add_sample<'a>(&mut self, mut path: impl Iterator<Item=&'a str>) {
+    //     self.total += 1;
+    //     match path.next() {
+    //         Some(p) => {
+    //             self.children.raw_entry_mut().from_key(p).or_insert_with(|| (p.to_owned(), SampleTree::new())).1.add_sample(path);
+    //         },
+    //         None => {},
+    //     }   
+    // }
+
+    pub fn add(&mut self, other: &Self) {
+        self.total += other.total;
+        for (k, v) in &other.children {
+            self.get_or_create_child(k.as_str()).add(v)
+        }
+    }
+
+    pub fn sub(&mut self, other: &Self) {
+        self.total -= other.total;
+        if self.total == 0 {
+            self.children.clear();
+            return
+        }
+        for (k, v) in &other.children {
+            match self.children.get_mut(k.as_str()) {
+                Some(c) => c.sub(v),
+                None => {},
+            }
+        }
+    }
+
+    fn get_or_create_child(&mut self, k: &str) -> &mut Self {
+        self.children.raw_entry_mut().from_key(k).or_insert_with(|| (k.to_owned(), SampleTree::new())).1
+    }
+
+    pub fn add_sample<'a>(&mut self, mut path: impl Iterator<Item=&'a str>) {
+        self.total += 1;
+        match path.next() {
+            Some(p) => {
+                self.get_or_create_child(p).add_sample(path)
+            },
+            None => {},
+        }   
+    }
+
+    pub fn add_samples<'a>(&mut self, mut path: impl Iterator<Item=&'a str>, n: u64) {
+        self.total += n;
+        match path.next() {
+            Some(p) => {
+                self.get_or_create_child(p).add_samples(path, n)
+            },
+            None => {},
+        }   
+    }
+
+ 
+
+    fn print_internal<W: fmt::Write>(&self, w: &mut W, total_samples: u64, bytes_per_sample:f64, min_disk_fraction: Option<f64>, name: &str, depth: usize) -> fmt::Result {
+        let disk_fraction = (self.total as f64) / (total_samples as f64);
+        // let disk_bytes = (total_length as f64 * disk_fraction) as u64;
+        let disk_bytes = (self.total as f64 * bytes_per_sample).round() as u64;
+
+        match min_disk_fraction {
+            Some(min_disk_fraction) if disk_fraction < min_disk_fraction => return Ok(()),
+            _ => {},
+        }
+
+        let path = { 
+            let mut path =  String::new();
+            for i in 0..depth {
+                path.push_str(" ");
+            }
+            path.push('/');
+            path.push_str(name);
+            path
+        };
+
+        writeln!(w, "{:60} {:>8} {:>4.1}% {:>16}", path, self.total,  disk_fraction * 100.0, bytesize::to_string(disk_bytes, true))?;
+
+        
+        let mut c: Vec<_> = self.children.iter().collect();
+        c.sort_by_key(|(_,v)| std::cmp::Reverse(v.total));
+        for (k,v) in &c {
+            v.print_internal(w, total_samples, bytes_per_sample, min_disk_fraction, k, depth+1)?;    
+        }
+
+        Ok(())
+    }
+
+    pub fn print<W: fmt::Write>(&self, w: &mut W, total_samples: u64, bytes_per_sample: f64, min_disk_fraction: Option<f64>) -> fmt::Result {
+        self.print_internal(w, total_samples, bytes_per_sample, min_disk_fraction, "", 0)
+    }
+}
